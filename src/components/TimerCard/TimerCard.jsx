d0d884a864fd03ca8c1aef5dbf118c1a4249f7dd
Tanish Mishra <tanishmishra97@gmail.com>
2024-01-18 00:15:00 +0530

    feat: added browser entertainment page

--- src/components/TimerCard/TimerCard.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import styles from './TimerCard.module.css'
-const TimerCard = () => {
-  return (
-    <div className={styles.card}></div>
-  )
-}
-
-export default TimerCard
--- src/pages/GenrePage/GenrePage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Genre from '../../components/Genre/Genre'
-const GenrePage = () => {
-  return (
-      <Genre/>
-  )
-}
-
-export default GenrePage
