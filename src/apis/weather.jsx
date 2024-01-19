d393c8f14f85fcdeb6bfd99e72d33863efa734b0
Tanish Mishra <tanishmishra97@gmail.com>
2024-01-20 02:36:46 +0530

    fix: api env

--- src/apis/weather.jsx
+++ src/apis/weather.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 export const getWeatherDetails = async () => {
   const DEFAULT_LOCATION = "lucknow";
   try {
-     const reqUrl = `http://api.weatherapi.com/v1/current.json?q=${DEFAULT_LOCATION}&key=${process.env.VITE_WEATHER_API_KEY}`
+     const reqUrl = `http://api.weatherapi.com/v1/current.json?q=${DEFAULT_LOCATION}&key=${import.meta.env.VITE_WEATHER_API_KEY}`
      const response = await axios.get(reqUrl);
      return response.data;
   } catch (error) {
